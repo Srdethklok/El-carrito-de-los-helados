91466e8f6718a9c5de31b547d77b06a7438b559f
Adrián <la.muerte.de.pandora@gmail.com>
2018-09-22 15:46:06 +0200

    utilización de los operadores booleanos and, or, true and false
    y el truco de las mayusculas (.upper())

--- helados.py
+++ helados.py
@@ -1,11 +1,11 @@
-apetece_helado_in = input("Te apetece un helado? (Si / No): ")
-tienes_dinero_in = input("Tienes dinero para un helado? (Si / No): ")
-esta_tu_tia_in = input("Esta tu tia? (Si / No): ")
-esta_el_senor_de_los_helados_in = input("Esta el señor de los helados? (Si / No): ")
+apetece_helado_in = input("Te apetece un helado? (Si / No): ").upper()
+tienes_dinero_in = input("Tienes dinero para un helado? (Si / No): ").upper()
+esta_tu_tia_in = input("Esta tu tia? (Si / No): ").upper()
+esta_el_senor_de_los_helados_in = input("Esta el señor de los helados? (Si / No): ").upper()
 
-apetece_helado = apetece_helado_in == "Si"
-tienes_dinero = tienes_dinero_in or esta_tu_tia_in == "Si"
-esta_el_senor_de_los_helados = esta_el_senor_de_los_helados_in == "Si"
+apetece_helado = apetece_helado_in == "SI"
+tienes_dinero = tienes_dinero_in or esta_tu_tia_in == "SI"
+esta_el_senor_de_los_helados = esta_el_senor_de_los_helados_in == "SI"
 
 
 if apetece_helado and tienes_dinero and esta_el_senor_de_los_helados:
